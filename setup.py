186c83cf92a3c5913a618aba2ac2920ea7ede273
Bertrand Chenal <bchenal@fundp.ac.be>
2013-07-19 13:59:49 +0200

    Increment version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 from distutils.core import setup
 
 setup(name='Menger',
-      version='1.6.1',
+      version='1.6.2',
       description='Statistics storage',
       author='Bertrand Chenal',
       author_email='bertrandchenal@gmail.com',
