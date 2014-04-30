6a5e98ca3946664ee0d74937fdb17f644429d106
Bertrand Chenal <bertrand@adimian.com>
2014-04-30 10:40:13 +0200

    Fetch is now dice

--- tests/test-mng.sh
+++ tests/test-mng.sh
@@ -4,8 +4,8 @@ export PATH=../bin/:$PATH
 
 mng load example.json
 assert "echo $?" 0
-assert "mng fetch" "total\n65"
-assert "mng fetch category=Jewellery" "total\n5"
-assert 'mng fetch category="Electronics/Television & Video"' "total\n20"
+assert "mng dice" "total\n65"
+assert "mng dice category=Jewellery" "total\n5"
+assert 'mng dice category="Electronics/Television & Video"' "total\n20"
 assert "mng drill category=Electronics" "Electronics/Camera & Photo\nElectronics/Computers\nElectronics/Television & Video"
 assert_end
